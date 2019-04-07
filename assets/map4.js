a4b0ec4bb309fb4b83739f3ec3450af4ab4e9de2
Kyla <kylapurcell18@gmail.com>
2019-04-07 01:48:48 -0700

    added parking and restaurant feature

---
+++ assets/map4.js
@@ -0,0 +1,32 @@
+function initMap() {
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 12,
+      center: {lat: 49.282730, lng: -123.120735},
+      mapTypeId: "roadmap"
+    });
+    setMarkers(map);
+    setOrgMarkers(map);
+  }
+  
+  function setMarkers(map) { var dbRef= firebase.database().ref('Restaurants');
+  dbRef.on('child_added', function(snapshot) {
+    var snap = snapshot.val();
+    let image = "images/food.png"
+    new google.maps.Marker({
+      position: {lat: parseFloat(snap.Latitude), lng: parseFloat(snap.Longitude)},
+      map: map,
+      icon: image
+      });
+    });
+  }
+  
+  function setOrgMarkers(map) {
+    var locationRef = firebase.database().ref('Location');
+    locationRef.on('child_added', function(snapshot) {
+      var snap = snapshot.val();
+      new google.maps.Marker({
+        position: {lat: parseFloat(snap.lat), lng: parseFloat(snap.lng)},
+        map: map
+        });
+      });
+    }
